dc59d9530cb7215da886a2193a8f2a65f40080f0
chapkovski <chapkovski@gmail.com>
2017-04-16 16:18:24 -0400

    an attempt to add a periodic task dynamically

--- rango/tasks.py
+++ rango/tasks.py
@@ -3,6 +3,14 @@ from celery import Celery
 app = Celery( broker='redis://localhost:6379/0')
 
 
+
+
 @app.task
 def add(x, y):
+    print('HURRRA!!!!!!')
     return x + y
+
+@app.task
+def filka(arg):
+    print('WHATEVER!!!!!')
+    # return x + y
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,36 @@
 from __future__ import absolute_import, unicode_literals
 from django.shortcuts import render
-
+from django_celery_beat.models import PeriodicTask, PeriodicTasks
+# print(dir(django_celery_beat))
 # Create your views here.
 from django.http import HttpResponse
 import random
+from celery import app
+from datetime import timedelta
 
 from .tasks import *
 
 def index(request):
-    add.delay(4, 4)
+    for p in PeriodicTask.objects.all():
+        print(p)
+    ppp10 = PeriodicTask.objects.get(name='add every 10')
+    print('PPPPPPP~~!)!)!)!10', ppp10.enabled)
+    ppp10.enabled = not ppp10.enabled
+    print('PPPPPPP~~!)!)!)!10', ppp10.enabled)
+    ppp10.save()
+    PeriodicTasks.changed(ppp10)
+    print('#####', len(PeriodicTask.objects.all()))
+
+    # add.delay(4, 4)
+    # filka.delay('asdf')
+    # print('^^^^^^^^^^^^^', app.conf)
+    # app.conf.update(
+    #     CELERYBEAT_SCHEDULE={
+    #         'perminute': {
+    #             'task': 'tasks.add',
+    #             'schedule': timedelta(seconds=3),
+    #             'args': (1, 1)
+    #         }
+    #     }
+    # )
     return HttpResponse("Rango says hey there partner!")
